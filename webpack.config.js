e0ca7ec199fa28bde0fe267e235ad3b148a983be
xiumubai <1547702880@qq.com>
2022-05-01 17:03:59 +0800

    feat: 挂载插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const RunPlugin = require('./lib/plugins/runPlugin');
+const DonePlugin = require('./lib/plugins/DonePlugin');
+
 module.exports = {
   mode: 'development',
   devtool: false,
@@ -7,4 +10,5 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve('dist'),
   },
+  plugins: [new RunPlugin(), new DonePlugin()],
 };
