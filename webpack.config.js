0fa4d650caff90f070e687bddf22d26ab13b7363
xiumubai <1547702880@qq.com>
2022-05-01 18:02:50 +0800

    feat: 确定文件的路径

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,22 @@ module.exports = {
   // entry: './src/index.js',
   entry: {
     foo: './src/foo.js',
-    bar: ',/src/bar.js',
+    bar: './src/bar.js',
   },
   output: {
     filename: '[name].js',
     path: path.resolve('dist'),
   },
   plugins: [new RunPlugin(), new DonePlugin()],
+  modules: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          path.resolve(__dirname, 'lib/loaders', 'loader1.js'),
+          path.resolve(__dirname, 'lib/loaders', 'loader2.js'),
+        ],
+      },
+    ],
+  },
 };
