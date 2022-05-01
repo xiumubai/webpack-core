21f33ba0fe83ac3eeb70e237ad8d8618ed202dca
xiumubai <1547702880@qq.com>
2022-05-01 17:36:57 +0800

    feat: 确定入口模块

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,11 @@ const DonePlugin = require('./lib/plugins/DonePlugin');
 module.exports = {
   mode: 'development',
   devtool: false,
-  entry: './src/index.js',
+  // entry: './src/index.js',
+  entry: {
+    foo: './src/foo.js',
+    bar: ',/src/bar.js',
+  },
   output: {
     filename: '[name].js',
     path: path.resolve('dist'),
